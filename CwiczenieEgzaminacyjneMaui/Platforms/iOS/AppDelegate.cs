6bf785b765a345027b1022b3f7a371db38006035
antodereq <Antek.Ulaczyk@wp.pl>
2026-06-01 15:58:08 +0200

    Dodaj pliki projektów.

---
+++ CwiczenieEgzaminacyjneMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CwiczenieEgzaminacyjneMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
